c461c5a0c6090e251c1faa17b2d2ac73f7473364
Sean Lawlor <lawlorseanr@gmail.com>
2021-06-30 18:33:51 -0700

    Minor changes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import store from './store/store.js';
 //  the rest of your app.
 ReactDOM.render(
   <Provider store={store}>
-    <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />
+    <App />
   </Provider>,
   document.getElementById('app')
 );
